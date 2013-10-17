845561466be8128ab9999e3d8953beea466e7992
onowhid <info@sewede.de>
2013-10-17 17:54:30 +0000

    middle of third lesson

--- public/index.php
+++ public/index.php
@@ -1,7 +1,37 @@
 <?php
 
-//	contrrollwe
+//	contrroller
 
+	require_once("../classes/class.Authentication.php");
+
+	session_start();
+
+	if ($_SESSION['LoggedIn'] == 1) {
+		// logged in
+		echo "You are logged in";
+	}
+	else {
+		
+		if ($_POST['submit'] == 'Submit') {
+			// they submitted the login form		
+
+			if ($_POST['username'] == 'Sebastian' && $_POST['password'] == 'blahblah') {
+				echo "logged in!";
+				$_SESSION['loggedIn'] = 1;
+			} else {
+				echo "wrong username!";
+				$_SESSION['loggedIn'] = 0;
+			}	
+			
+		} else {
+		}
+		//not logged in 
+		$content = file_get_contents("../templates/loginform.html");
+		echo $content;
+
+		// $_SESSION['LoggedIn'] = 1;
+	}
+	die;
 	if ($_SERVER['REQUEST_URI'] == '/') {
 		// hompage
 		$content = file_get_contents("../templates/index.html");
